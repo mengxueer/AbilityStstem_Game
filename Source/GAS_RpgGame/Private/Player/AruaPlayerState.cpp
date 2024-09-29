28055a7ad3c3929ca21b0b22d3b2ff5fa1512729
xuemenger <1053289488@qq.com>
2024-09-29 10:17:47 +0800

    创建gamestatus,设置AbilitySystemcompoent基础设置

---
+++ Source/GAS_RpgGame/Private/Player/AruaPlayerState.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/AruaPlayerState.h"
+
+AAruaPlayerState::AAruaPlayerState() {
+	NetUpdateFrequency=100.f;//网络更新频率
+}
