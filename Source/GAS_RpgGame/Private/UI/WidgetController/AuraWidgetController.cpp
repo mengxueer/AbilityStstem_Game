4b3c2b59c0c1ec2c8784527f5d426e95ffca73de
dangjiameng <1053289488@qq.com>
2024-10-01 18:42:42 +0800

    增加属性绑定,属性监听回调

---
+++ Source/GAS_RpgGame/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
+void UAuraWidgetController::SetWidgetControllerParams(const FWidgetControllerParams& WCParams)
+{
+	PlayController=WCParams.PlayerController;
+	PlayState=WCParams.PlayerState;
+	AbilitySystemComponent=WCParams.AbilitySystemComponent;
+	AttributeSet=WCParams.AttributeSet;
+}
+
+void UAuraWidgetController::BrodcastInitValues()
+{
+}
+
+void UAuraWidgetController::BindCallbacksToDependencies()
+{
+}
---
+++ Source/GAS_RpgGame/Public/UI/WidgetController/OverlayWidgetController.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UI/WidgetController/AuraWidgetController.h"
+#include "OverlayWidgetController.generated.h"
+
+
+struct FOnAttributeChangeData;
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature,float,NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FonManaChanggedSignature,float,Mana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FonMaxManaChanggedSignature,float,MaxMana);
+/**
+/**
+ * 
+ */
+UCLASS(BlueprintType,Blueprintable)
+class GAS_RPGGAME_API UOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+public:
+	virtual void BrodcastInitValues() override;
+    virtual void BindCallbacksToDependencies() override;
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnHealthChangedSignature OnHealthChanged;
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FOnMaxHealthChangedSignature OnMaxHealthChanged;
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FonManaChanggedSignature OnManaChanged;
+	UPROPERTY(BlueprintAssignable,Category="GAS|Attributes")
+	FonMaxManaChanggedSignature OnMaxManaChanged;
+	
+protected:
+	void HealthChanged(const FOnAttributeChangeData& Data) const;
+	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
+     //法力值
+	void ManaChanged(const FOnAttributeChangeData& Data)const;
+	void MaxManaChanged(const FOnAttributeChangeData& Data)const;
+	
+};
